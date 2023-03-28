1d645cb96ef7a30f05fc08cc137290b839372104
FRC Team FMWill 7576 <yigit.olc@hotmail.com>
2023-03-28 12:34:10 +0300

    zerogyro added

--- src/main/java/frc/robot/autos/doNothing.java
+++ src/main/java/frc/robot/autos/doNothing.java
@@ -1,10 +1,9 @@
 package frc.robot.autos;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
+
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
 
 public class doNothing extends SequentialCommandGroup {
@@ -14,6 +13,7 @@ public class doNothing extends SequentialCommandGroup {
 
     addCommands(
       new InstantCommand(() -> s_Swerve.resetModulesToAbsolute()),
+      new InstantCommand(() -> s_Swerve.zeroGyro()),
      new InstantCommand(() -> s_Swerve.drive(
             new Translation2d(0, 0),
             0,
