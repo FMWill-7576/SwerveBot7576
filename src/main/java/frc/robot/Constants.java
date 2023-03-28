ad3440c43c65bc30adaf7c88ec781e1efb46a773
FRC Team FMWill 7576 <yigit.olc@hotmail.com>
2023-03-28 12:32:59 +0300

    edited offset

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -131,7 +131,7 @@ public final class Constants {
       public static final int driveMotorID = 1;
       public static final int angleMotorID = 2;
       public static final int canCoderID = 1;
-      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(199.2);
+      public static final Rotation2d angleOffset = Rotation2d.fromDegrees(199.5);
       public static final SwerveModuleConstants constants =
           new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
     }
