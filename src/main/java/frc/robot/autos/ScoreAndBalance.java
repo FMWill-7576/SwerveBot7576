7bed79c1ac1f3434057f8c91c0f878b09923c38e
FRC Team FMWill 7576 <yigit.olc@hotmail.com>
2023-03-28 12:40:12 +0300

    removed unused stuff

--- src/main/java/frc/robot/autos/ScoreAndBalance.java
+++ src/main/java/frc/robot/autos/ScoreAndBalance.java
@@ -4,17 +4,6 @@
 
 package frc.robot.autos;
 
-import java.util.List;
-
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
@@ -36,48 +25,8 @@ import frc.robot.subsystems.Slider;
 public class ScoreAndBalance extends SequentialCommandGroup {
   
   public ScoreAndBalance(Swerve s_Swerve, Gripper s_Gripper, VictorArm s_VictorArm, Slider s_Slider) {
-    TrajectoryConfig config =
-        new TrajectoryConfig(
-                Constants.AutoConstants.kMaxSpeedMetersPerSecond,
-                Constants.AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            .setKinematics(Constants.Swerve.swerveKinematics);
 
-
-
-
-Trajectory exampleTrajectory =
-        TrajectoryGenerator.generateTrajectory(
-            // Start at the origin facing the +X direction
-            new Pose2d(0.0, 0.0, new Rotation2d(Math.toRadians(180.0))),
-            // Pass through the  waypoints
-            List.of(    
-                new Translation2d(0.0, 0.01), 
-                new Translation2d(1.5, 0.0)
-                ),
-            // end
-            new Pose2d(2.0, 0.0, new Rotation2d(Math.toRadians(180.0))),
-            config);
-        var thetaController =
-        new ProfiledPIDController(
-            Constants.AutoConstants.kPThetaController,
-            0.0,
-            0.1,
-            Constants.AutoConstants.kThetaControllerConstraints);
-    thetaController.enableContinuousInput(-Math.PI, Math.PI);
-
-
-    SwerveControllerCommand swerveControllerCommand =
-        new SwerveControllerCommand(
-            exampleTrajectory,
-            s_Swerve::getPose,
-            Constants.Swerve.swerveKinematics,
-            new PIDController(Constants.AutoConstants.kPXController, 0.0, 0.15),
-            new PIDController(Constants.AutoConstants.kPYController, 0.0, 0.15),
-            thetaController,
-            s_Swerve::setModuleStates,
-            s_Swerve);
-
-            
+          
 
     addCommands(
         /* Commands.run(() -> s_VictorArm.armUp()).asProxy().withTimeout(0.6),
@@ -88,18 +37,12 @@ Trajectory exampleTrajectory =
        // s_Slider.run(() -> s_Slider.slideTesting()).withTimeout(0.4) ,
 
        s_Gripper.run(() -> s_Gripper.outake()).withTimeout(0.9),
-        new InstantCommand(() -> s_Gripper.stop()),
+       new InstantCommand(() -> s_Gripper.stop()),
         
-         new InstantCommand(() -> s_Swerve.resetModulesToAbsolute()),
-        
-        // new InstantCommand(() -> s_Swerve.invertGyro()),
+       new InstantCommand(() -> s_Swerve.resetModulesToAbsolute()),
        new InstantCommand(() -> s_Swerve.zeroGyro()),
        new DriveForward2(s_Swerve),
        new WaitCommand(1.0),
-      //new InstantCommand(() -> s_Swerve.resetOdometry(exampleTrajectory.getInitialPose())),
-  
-       //swerveControllerCommand
-       //new DriveBack(s_Swerve),
        new BalanceCommand2(s_Swerve)
        
    
